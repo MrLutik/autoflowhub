536eb48c1e11d7f4acb207c3ac50e8f0a5b3df45
mrlutik <yaievgeniy@gmail.com>
2023-05-29 17:04:26 +0200

    Add txgen to cli

--- cmd/autoflowhub/main.go
+++ cmd/autoflowhub/main.go
@@ -4,12 +4,13 @@ import (
 	tool "github.com/mrlutik/autoflowhub/internal/cli"
 	"github.com/mrlutik/autoflowhub/pkg/keygen/keygencmd"
 	"github.com/mrlutik/autoflowhub/pkg/tokenspread/tokenspreadcmd"
+	"github.com/mrlutik/autoflowhub/pkg/txgen/txgencmd"
 	"github.com/mrlutik/autoflowhub/pkg/txsgen/txsgencmd"
 	"github.com/spf13/cobra"
 )
 
 func main() {
-	cmds := []*cobra.Command{txsgencmd.New(), keygencmd.New(), tokenspreadcmd.New()}
+	cmds := []*cobra.Command{txsgencmd.New(), keygencmd.New(), tokenspreadcmd.New(), txgencmd.New()}
 	cli := tool.NewCLI(cmds)
 	cobra.CheckErr(cli.Execute())
 }
